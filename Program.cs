a239ae1fa6bf4b19e66c2b3c06c4195a0e14cf37
Mubashir Siddique <mubashir.siddique.ubit@gmail.com>
2021-04-06 15:48:24 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test TestObject = new Test();
+
+            var InputString = "Shah";
+            var CharToReturn =  TestObject.CheckRepeatition(InputString);
+
+            if (CharToReturn == '\0')
+            {
+                Console.WriteLine("There is no Duplicate character in the Given String");
+            }
+            Console.WriteLine("The First Duplicated Character was {0}", CharToReturn);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharRepetition
+{
+    class Test
+    {
+        public char CheckRepeatition(String Argument)
+        {
+            char CharToReturn = '\0';       // 1
+            String Checked = "";            // 1
+            try
+            {
+                for (int i = 0; i < Argument.Length; i++)   // 1 + (N+1) + N
+                {
+                    if (Checked.Contains(Argument[i]))  // 1
+                    {
+                        CharToReturn = Argument[i];     // 1
+                        break;
+                    }
+                    Checked = String.Format("{0}{1}", Checked, Argument.ToLower()[i]); // 1
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+
+            return CharToReturn;    //  1
+        }
+    }
+}
